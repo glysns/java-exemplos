901eb1526ecab5f05d079594d280420e9d86f541
Gleyson Gama <gleyson.sampaio@gama.academy>
2021-04-29 07:56:03 -0300

    iniciando o projeto resource repository

--- spring/spring-resource-repository/src/main/resources/db/migration/V7__apl_avaliacao_vw_questao.sql
+++
@@ -1,5 +0,0 @@
-DROP VIEW IF EXISTS apl_avaliacao.vw_questao;
-
-CREATE OR REPLACE VIEW apl_avaliacao.vw_questao AS
-select tq.*, tt.nome as tema_nome, tt.sigla as tema_sigla 
-from apl_avaliacao.tab_questao tq inner join apl_avaliacao.tab_tema tt on tq.cd_tema = tt.id; 
--- spring/spring-resource-repository/src/main/resources/sql.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO apl_acesso.tab_usuario
-(fl_bloqueado, "password", username, nome)
-VALUES(false, '$2a$10$LmJcxUb53E6RDy4AjXeWEegoMRvmtstMhr.KdkybhZEFjVemy2dA2', 'educa', 'EDUCA - ADMIN');
-
--- {"password": "Educ@","username": "educa"}
